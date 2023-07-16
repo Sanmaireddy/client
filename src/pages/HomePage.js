4240014917dff179c9572ac0b7ffacb47431059c
Sanmai Reddy <106271316+Sanmaireddy@users.noreply.github.com>
2023-07-16 14:58:15 +0530

    Add files via upload

---
+++ src/pages/HomePage.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from "react";
+import Layout from "../components/Layout/Layout";
+import { useAuth } from "../context/auth";
+import axios from "axios";
+import { Checkbox } from "antd";
+
+const HomePage = () => {
+  const [auth, setAuth] = useAuth();
+  const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+
+  //get categories
+  const getAllCategory = async () => {
+    try {
+      const { data } = await axios.get(
+        `${process.env.REACT_APP_API}/api/v1/category/get-category`
+      );
+      if (data?.success) {
+        setCategories(data?.category);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getAllCategory();
+  }, []);
+
+  //get products
+  const getAllProducts = async () => {
+    try {
+      const { data } = await axios.get(
+        `${process.env.REACT_APP_API}/api/v1/product/get-product`
+      );
+      setProducts(data.products);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getAllProducts();
+  }, []);
+  return (
+    <Layout>
+      <div className="row mt-3">
+        <div className="col-md-3">
+          <h4 className="text-center">Filter By Category</h4>
+        </div>
+        <div className="col-md-9">
+          <h1 className="text-center">ALL Products</h1>
+          <div className="d-flex flex-wrap">
+            {products?.map((p) => (
+              <div className="card m-2" style={{ width: "18rem" }}>
+                <img
+                  className="card-img-top"
+                  src={`${process.env.REACT_APP_API}/api/v1/product/product-photo/${p._id}`}
+                  alt={p.name}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{p.name}</h5>
+                  <p className="card-text">{p.description}</p>
+                  <button className="btn btn-primary ms-1">More details</button>
+                  <button className="btn btn-secondary ms-1">
+                    Add to cart
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default HomePage;
